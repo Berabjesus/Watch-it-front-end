3831dce70a7d4e4816de6ebe0d7ed3c71be7c27f
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-04-09 09:01:44 +0300

    ADD proptypes for item.js

--- src/components/home/item.js
+++ src/components/home/item.js
@@ -1,19 +1,24 @@
-/* eslint-disable */
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link, Redirect } from 'react-router-dom';
-import itemCss from './item.module.css';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Item = ({id,title, date}) => {
-  const formated_date = new Date(date)
+const Item = ({ id, title, date }) => {
+  const formatedDate = new Date(date);
   const loginStatus = useSelector((state) => state.session);
   return (
     <Link to={`/view/${loginStatus.username}/${id}`} className="card d-flex p-2 mt-1 mb-2 shadow-sm">
       <p>{id}</p>
       <p>{title}</p>
-      <p>{formated_date.toDateString()}</p>
+      <p>{formatedDate.toDateString()}</p>
     </Link>
   );
-}
+};
+
+Item.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+};
 
 export default Item;
